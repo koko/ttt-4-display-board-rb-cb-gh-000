a306773ac3c8ac788bdff84b8c20fa7d3b117bc5
Ronnie Garduño <koko@rpg-free.com>
2017-06-13 22:23:16 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  board.each_with_index { |e, i|
+    if i % 3 != 2
+      print " #{e} |"
+    elsif i == 0
+      print " #{e} |"
+    end
+    if i % 3 == 2
+      puts " #{e} "
+      puts "-----------"
+    end
+  }
+end
